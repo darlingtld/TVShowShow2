0a64ab846aebe9b3fc7d7dc0c70cf61e88faa1df
Lingda Tang <lingda@splunk.com>
2017-06-26 15:54:33 +0800

    add styles to the search form

--- src/main/frontend/tvshowapp/src/app/app.module.ts
+++ src/main/frontend/tvshowapp/src/app/app.module.ts
@@ -2,9 +2,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {SearchFormComponent} from './search-form/search-form.component';
-import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
   {path: 'search', component: SearchFormComponent},
