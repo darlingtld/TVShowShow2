bb55c2915afad851a9952a0ac5a76f7fa92436db
Lingda Tang <lingda@splunk.com>
2017-07-08 12:11:24 +0800

    add elasticsearch JAVA REST client to gradle

--- src/test/java/lingda/service/crawler/impl/ShowCrawlerMeijuttImplTest.java
+++ src/test/java/lingda/service/crawler/impl/ShowCrawlerMeijuttImplTest.java
@@ -3,6 +3,19 @@ package lingda.service.crawler.impl;
 import lingda.model.dto.DownLoadLink;
 import lingda.model.dto.TVShowSearchResult;
 import lingda.model.pojo.TVShow;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +24,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -67,6 +82,37 @@ public class ShowCrawlerMeijuttImplTest {
     @Test
     public void shouldGetDownloadLinks() {
         List<DownLoadLink> downloadLinkList = showCrawlerMeijuttImpl.searchDownloadLinks("http://www.meijutt.com/content/meiju131.html");
-        downloadLinkList.forEach(System.out::println);
+        assertThat(downloadLinkList, not(IsEmptyCollection.empty()));
+    }
+
+    @Test
+    public void testElasticSearchIndex() throws IOException {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "changeme"));
+
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+                .build();
+
+        Response response = restClient.performRequest("GET", "/",
+                Collections.singletonMap("pretty", "true"));
+        System.out.println(EntityUtils.toString(response.getEntity()));
+
+//index a document
+        HttpEntity entity = new NStringEntity(
+                "{\n" +
+                        "    \"user\" : \"kimchy\",\n" +
+                        "    \"post_date\" : \"2009-11-15T14:12:12\",\n" +
+                        "    \"message\" : \"trying out Elasticsearch\"\n" +
+                        "}", ContentType.APPLICATION_JSON);
+
+        Response indexResponse = restClient.performRequest(
+                "PUT",
+                "/twitter/tweet/1",
+                Collections.<String, String>emptyMap(),
+                entity);
+        System.out.println(indexResponse);
+        restClient.close();
     }
 }
